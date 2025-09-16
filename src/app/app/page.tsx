6078a6f9c71230a3544cb4a4d4a87ddd0baf1349
wellitsabhi <abhishekongit@gmail.com>
2025-09-16 21:39:10 +0530

    clerk webhooks

---
+++ src/app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function AppPage () {
+    return (
+        <div className="p-4 space-y-4 gap-4">
+            <div className="flex items-center gap-4">
+                <h1>Huuu</h1>
+            </div>
+        </div>
+    )
+}
